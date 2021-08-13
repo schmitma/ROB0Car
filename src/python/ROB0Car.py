b46f1bf56ee7952ad100e070b4b75eaf696e30bd
schmitma <max.schmitt@gmx.de>
2021-08-13 21:35:28 +0200

    Encapsulated control algorithm for ROB0Car in python classes.

---
+++ src/python/ROB0Car.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+from motor import Motor
+import time
+
+class ROB0Car:
+    def __init__ (self):
+        self.leftMotor = Motor(4, 50)
+        self.rightMotor = Motor(5, 50)
+
+    def drive(self, speed):
+        self.leftMotor.set_motor_speed(speed)
+        self.rightMotor.set_motor_speed(speed)
+
+def main():
+    rob0car = ROB0Car()
+    
+    for s in range(0,100,10):
+        rob0car.drive(s)
+        time.sleep(1)
+
+    for s in range(100,0,-10):
+        rob0car.drive(s)
+        time.sleep(1)
---
+++ src/python/motor.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python3
+import pigpio
+import os
+
+class Motor:
+    '''A brushless DC motor driven by one pin on a Raspberry Pi.'''
+
+    def __init__ (self, pin, frequency=50):
+        #  Configure GPIO
+        if os.system ("pgrep pigpiod") != 0:
+            os.system("sudo pigpiod")
+
+        self._pi = pigpio.pi()  # Connect to PIGPIO-Daemon
+        self._pin = pin
+        self._frequency = frequency
+        self._speed = 0
+
+        self._pi.set_mode(pin, pigpio.OUTPUT)
+        self._pi.set_PWM_frequency(pin, frequency)
+        self._pi.set_PWM_range(pin, 100)  # Control PWM from 0 .. 100 %
+
+    def speedperc2dc(mot_speed_perc):
+        min_dc = 5      # Corresponds to 1 ms @ 50 Hz
+        max_dc = 10     # Corresponds to 2 ms @ 50 Hz
+        dc = min(max(7.5 + mot_speed_perc*2.5/100, min_dc), max_dc)
+        print('DutyCycle: ' + str(dc))
+
+    def arm(self):
+        self.set_motor_speed(0)
+
+    def set_motor_speed (self, speed):      
+        self._pi.set_PWM_dutycycle(self._pin, self.speedperc2dc(speed))
+        self._speed = speed
