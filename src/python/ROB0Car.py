27f3d39e5591b0d98d8838f864fd03e5b7068e15
schmitma <max.schmitt@gmx.de>
2021-08-13 21:59:01 +0200

    Added logging to ROB0Car.py.

--- src/python/ROB0Car.py
+++ src/python/ROB0Car.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
 from motor import Motor
 import time
+import logging
+import sys
+
+logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 
 class ROB0Car:
     def __init__ (self):
+        logging.debug("ROBOCar.__init__")
         self.leftMotor = Motor(4, 50)
         self.rightMotor = Motor(5, 50)
 
     def drive(self, speed):
+        logging.debug("ROBOCar.drive")
         self.leftMotor.set_motor_speed(speed)
         self.rightMotor.set_motor_speed(speed)
 
