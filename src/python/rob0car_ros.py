4111de15346a9342392f63988e63ddf75ce144ab
schmitma <max.schmitt@gmx.de>
2021-08-16 13:04:29 +0200

    Added rob0car_ros.py.

---
+++ src/python/rob0car_ros.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python3
+import pigpio
+import os
+import logging
+import sys
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Int32
+from motor import Motor
+from ROB0Car import ROB0Car
+
+logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+
+class MinimalSubscriber(Node):
+    def __init__(self):
+        super().__init__('minimal_subscriber')
+        self.subsricpiton = self.create_subscription(
+            Int32,
+            'steering',
+            self.listener_callback,
+            10)
+        self.subscription
+        self.rob0car = ROB0Car()
+
+    def listener_callback(self, msg):
+        steering_dc = msg.data
+        self.rob0car.steering.set_steering_dc(steering_dc)
+
+    def main(args=None):
+        # initialize the rob0car node
+        rclpy.init(args=args)
+        minimal_subscriber = MinimalSubscriber()
+
+        # wait for incoming commands
+        rclpy.spin(minimal_subscriber)
+
+        # Interrupt detected, shut down
+        minimal_subscriber.rob0car.stop()
+        minimal_subscriber.destroy_node()
+        rclpy.shutdown()
+
+    if __name__ == '__main__':
+        main()
