cf9b72c9633d8c6558097bf8f2b3356bd7161074
schmitma <max.schmitt@gmx.de>
2021-08-23 13:55:07 +0200

    Extended example ultrasonic_pcf8574.py.

--- src/examples/ultrasonic_pcf8574.py
+++ src/examples/ultrasonic_pcf8574.py
@@ -9,38 +9,60 @@ pcf8574_i2c_addr = 0x27
 
 P0 = 0x01
 P1 = 0x02
+P2 = 0x04
+P3 = 0x08
+P4 = 0x10
+P5 = 0x20
+P6 = 0x40
+P7 = 0x80
 
-GPIO.setmode(GPIO.BOARD)
-trig = [11] #, 36]
-echo = [13] #, 38]
+trig = P0
+echo = P4
 
-for i in range(len(trig)):
-	GPIO.setup(echo[i], GPIO.IN)
-	GPIO.setup(trig[i], GPIO.OUT)
+bus.write_byte_data(pcf8574_i2c_addr, 0x00)
 
-def meas_dist(n):
-	logging.debug("meas_dist()")
+# GPIO.setmode(GPIO.BOARD)
+# trig = [11] #, 36]
+# echo = [13] #, 38]
 
-	# print("Trigger Pin: ", trig[n], 
-	#   "\nEcho Pin: ", echo[n])
-	GPIO.output(trig[n], True)
-	time.sleep(0.00001) # 10 Mikrosekunden
-	GPIO.output(trig[n], False)
-	b = bus.read_byte_data(pcf8574_i2c_addr)
+# for i in range(len(trig)):
+#     GPIO.setup(echo[i], GPIO.IN)
+#     GPIO.setup(trig[i], GPIO.OUT)
 
-	while (b & P0):
-		pass
-	start = time.time()
+def meas_dist():
+    logging.debug("meas_dist()")
 
-	while not (b & P0):
-		pass
-	end = time.time()
+    # print("Trigger Pin: ", trig[n], 
+    #   "\nEcho Pin: ", echo[n])
+    # GPIO.output(trig[n], True)
+    # time.sleep(0.00001) # 10 Mikrosekunden
+    # GPIO.output(trig[n], False)
 
-	distance = ((end - start) * 34300) / 2
-	print("Distance ", n, ": ", distance, " cm")
-	return distance
+    bus.write_byte_data(pcf8574_i2c_addr, trig)
+    time.sleep(0.00001) # 10 us
+    bus.write_byte_data(pcf8574_i2c_addr, trig)
+    logging.debug("meas_dist()")
+
+    b = bus.read_byte_data(pcf8574_i2c_addr)
+    logging.debug("Waiting for echo pin to turn HIGH")
+    while not (b & echo):
+        logging.debug(str(b))
+        b = bus.write_byte_data(pcf8574_i2c_addr)
+        pass
+    start = time.time()
+    
+    b = bus.read_byte_data(pcf8574_i2c_addr, trig)
+    logging.debug("Waiting for echo pin to turn LOW")
+    while (b & echo):
+        logging.debug(str(b))
+        b = bus.write_byte_data(pcf8574_i2c_addr, trig)
+        pass
+    end = time.time()
+
+    distance = ((end - start) * 34300) / 2
+    print("Distance: ", distance, " cm")
+    return distance
 
 while True:
-	for i in range(len(trig)):
-		meas_dist(i)
-		time.sleep(0.5)
+    meas_dist()
+    time.sleep(0.5)
