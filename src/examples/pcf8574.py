4506a3edc60f9827c578ea313a9f3d64b7f1fd85
schmitma <max.schmitt@gmx.de>
2021-08-23 10:49:26 +0200

    Extended example pcf8574.py.

--- src/examples/pcf8574.py
+++ src/examples/pcf8574.py
@@ -9,40 +9,27 @@ pcf8574_i2c_addr = 0x27
 
 P0 = 0x01
 P1 = 0x02
-
-GPIO.setmode(GPIO.BOARD)
-trig = [11] #, 36]
-echo = [13] #, 38]
-
-for i in range(len(trig)):
-    GPIO.setup(echo[i], GPIO.IN)
-    GPIO.setup(trig[i], GPIO.OUT)
-
-def meas_dist(n):
-    logging.debug("meas_dist()")
-
-    # print("Trigger Pin: ", trig[n], 
-    #   "\nEcho Pin: ", echo[n])
-    GPIO.output(trig[n], True)
-    time.sleep(0.00001) # 10 Mikrosekunden
-    GPIO.output(trig[n], False)
-    b = bus.read_byte_data(pcf8574_i2c_addr)
-
-    while (b & P0):
-        pass
-    start = time.time()
-
-    while not (b & P0):
-        pass
-    end = time.time()
-
-    distance = ((end - start) * 34300) / 2
-    print("Distance ", n, ": ", distance, " cm")
-    return distance
+P2 = 0x04
+P3 = 0x08
+P4 = 0x10
+P5 = 0x20
+P6 = 0x40
+P7 = 0x80
 
 while True:
     bus.write_byte(pcf8574_i2c_addr, P0)
-
     time.sleep(0.5)
     bus.write_byte(pcf8574_i2c_addr, P1)
+    time.sleep(0.5)
+    bus.write_byte(pcf8574_i2c_addr, P2)
+    time.sleep(0.5)
+    bus.write_byte(pcf8574_i2c_addr, P3)
+    time.sleep(0.5)
+    bus.write_byte(pcf8574_i2c_addr, P4)
+    time.sleep(0.5)
+    bus.write_byte(pcf8574_i2c_addr, P5)
+    time.sleep(0.5)
+    bus.write_byte(pcf8574_i2c_addr, P6)
+    time.sleep(0.5)
+    bus.write_byte(pcf8574_i2c_addr, P7)
     time.sleep(0.5)
