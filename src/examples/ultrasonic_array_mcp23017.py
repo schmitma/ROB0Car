324aba3ad161df021c4ea5e4a793886f877fb95b
schmitma <max.schmitt@gmx.de>
2021-08-29 15:34:08 +0200

    Changed sensor configuration in ultrasonic_array_mcp23017.py.

--- src/examples/ultrasonic_array_mcp23017.py
+++ src/examples/ultrasonic_array_mcp23017.py
@@ -178,8 +178,8 @@ class HCSR04Cluster:
         self._MODE = mode
         self._BANKING_MODE_IS_ACTIVE = 1 if (self._MODE | IOCON_BANK) else 0
         
-        self.sensors = [HCSR04("FRONT_RIGHT", 0)]#,
-                        #HCSR04("FRONT_MIDDLE", 1)]#,
+        self.sensors = [HCSR04("FRONT_RIGHT", 0),
+                        HCSR04("FRONT_MIDDLE", 1)]#,
                         #HCSR04("FRONT_LEFT", 2),
                         #HCSR04("LEFT", 3),
                         #HCSR04("REAR_LEFT", 4),
