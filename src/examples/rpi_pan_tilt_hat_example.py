6007011d7a61b39b4d90b4e232e458e4f0d14059
schmitma <max.schmitt@gmx.de>
2021-09-04 00:22:42 +0200

    Adapted rpi_pan_tilt_hat_example.py.

--- src/examples/rpi_pan_tilt_hat_example.py
+++ src/examples/rpi_pan_tilt_hat_example.py
@@ -2,24 +2,33 @@
 import time
 from pca9685 import PCA9685
 
+PAN = 0
+TILT = 1
+
 pca9685 = PCA9685()
 try:
     print ("This is an PCA9685 routine")    
     pca9685.setPWMFreq(50)
-    pca9685.setRotationAngle(1, 0)
+    pca9685.setRotationAngle(PAN, 90)
+    pca9685.setRotationAngle(TILT, 90)
     
     while True:
-        for i in range(10,170,1): 
-            pca9685.setRotationAngle(1, i)
-            if(i<80):
-                pca9685.setRotationAngle(0, i)   
-            time.sleep(0.1)
+        angle_tilt = int(input("Tilt angle [°]: "))
+        angle_pan = int(input("Pan angle [°]: "))
 
-        for i in range(170,10,-1): 
-            pca9685.setRotationAngle(1, i)   
-            if(i<80):
-                pca9685.setRotationAngle(0, i)            
-            time.sleep(0.1)
+        pca9685.setRotationAngle(PAN, angle_tilt)  
+        pca9685.setRotationAngle(TILT, angle_pan)  
+        # for i in range(10,170,1): 
+        #     pca9685.setRotationAngle(1, i)
+        #     if(i<80):
+        #         pca9685.setRotationAngle(0, i)   
+        #     time.sleep(0.1)
+
+        # for i in range(170,10,-1): 
+        #     pca9685.setRotationAngle(1, i)   
+        #     if(i<80):
+        #         pca9685.setRotationAngle(0, i)            
+        #     time.sleep(0.1)
 
 except:
     pca9685.exit_PCA9685()
